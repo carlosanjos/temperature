7abb1942b3b320d1caf4892bb7b598fd476a4d99
Carlos dos Anjos <carlos@goodtravelsoftware.com>
2019-04-30 14:43:32 +0100

    Initial Commit

---
+++ temperature/temperature.h
@@ -0,0 +1,19 @@
+//
+//  temperature.h
+//  temperature
+//
+//  Created by Carlos dos Anjos on 30/04/2019.
+//  Copyright © 2019 Carlos dos Anjos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for temperature.
+FOUNDATION_EXPORT double temperatureVersionNumber;
+
+//! Project version string for temperature.
+FOUNDATION_EXPORT const unsigned char temperatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <temperature/PublicHeader.h>
+
+
